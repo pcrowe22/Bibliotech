a73098551ea80989829653f614cfc0c251c08129
Patrick Crowe <pcrowe@hawk.iit.edu>
2019-12-11 16:51:51 -0600

    more accurately targets the options of the select list

--- JavaScript/binaries2.0.js
+++ JavaScript/binaries2.0.js
@@ -693,10 +693,10 @@ function chooseAxes(potAxes){
           }
         }
       //for adding an elemental power to the list of select options
-      var selectList = new Array(document.getElementById("power").options.text);
+      var selectList = new Array(document.getElementById("power").options);
       for (var j=0; j<axes[i].powers.length; j++) {
         for (var k=0; k<selectList.length; k++) {
-          if (!selectList[k].includes(axes[i].powers[j].name)) {
+        if (!selectList[k].text.includes(axes[i].powers[j].name)) {
             var newOption = document.createElement("option");
             newOption.text = axes[i].powers[j].name;
             newOption.value = axes[i].powers[j].name;
