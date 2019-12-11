78e9b95342dc0d9d4f917d1443a4d186a7a2c959
Patrick Crowe <pcrowe@hawk.iit.edu>
2019-12-11 14:44:53 -0600

    toLowerCase is not an option function

--- JavaScript/binaries2.0.js
+++ JavaScript/binaries2.0.js
@@ -686,7 +686,7 @@ function chooseAxes(potAxes){
           }
         }
       //for adding an elemental power to the list of select options
-      var selectList = new Array(document.getElementById("power").options.toLowerCase());
+      var selectList = new Array(document.getElementById("power").options.text);
       for (var j=0; j<axes[i].powers.length; j++) {
         if (!selectList.includes(axes[i].powers[j].name)) {
             var newOption = document.createElement("option");
