8dff550b52a8eb18759b47298e7482387514b130
Patrick Crowe <pcrowe@hawk.iit.edu>
2019-12-24 11:11:09 -0600

    Inserted an entry into sql

--- BinariesDatabase/BinariesDatabase/axesTable.sql
+++ BinariesDatabase/BinariesDatabase/axesTable.sql
@@ -4,3 +4,7 @@
     [ID1] TEXT NULL, 
     [ID2] TEXT NULL 
 )
+GO
+INSERT INTO axesTable (name, ID1, ID2) VALUES ("Light -- Dark ++ Light -- Dark", "Light -- Dark", "Light -- Dark")
+SELECT * FROM axesTable
+FOR JSON AUTO
